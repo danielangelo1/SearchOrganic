3d5c4847455e317745773e41023e44a57172ab8d
Pablo Leão <pablo_leao@hotmail.com>
2024-01-23 19:58:21 -0300

    fix: fixed footer links stylization

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./Footer.module.scss";
 import { InstagramLogo, TwitterLogo } from "@phosphor-icons/react";
 import { FacebookLogo } from "@phosphor-icons/react/dist/ssr";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -41,11 +42,10 @@ const Footer = () => {
         </nav>
         <div className={style.direitos}>
         <p>Search Organic © 2024</p>
-        <div className={style.redes_sociais}>
-          
-          <FacebookLogo size={"2rem"} />
-          <InstagramLogo size={"2rem"} />
-          <TwitterLogo size={"2rem"} />
+        <div className={style.redes_sociais}>   
+          <Link to="/"><FacebookLogo size={"2rem"} /></Link>
+          <Link to="/"><InstagramLogo size={"2rem"} /></Link>
+          <Link to="/"><TwitterLogo size={"2rem"} /></Link>
         </div>
         </div>
       </footer>
